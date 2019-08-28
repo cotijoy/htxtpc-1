086ebc434ecf67dac0826fb78eef737da2cb1c6f
root <whatreally@163.com>
2019-08-28 19:21:04 +0800

    新增注册的service

---
+++ src/main/java/com/magustek/com/htxtpc/register/service/RegisterModelService.java
@@ -0,0 +1,10 @@
+package com.magustek.com.htxtpc.register.service;
+
+import com.magustek.com.htxtpc.register.bean.RegisterModel;
+
+import java.util.Map;
+
+public interface RegisterModelService {
+
+    Map<String,Object> register(RegisterModel registerModel);
+}
