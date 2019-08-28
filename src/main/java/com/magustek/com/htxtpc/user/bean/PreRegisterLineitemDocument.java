283770f49d32142fadd03e4d45e0383df3e4270c
root <whatreally@163.com>
2019-08-28 11:21:41 +0800

    新增用户(预)注册行项目-资料 类

---
+++ src/main/java/com/magustek/com/htxtpc/user/bean/PreRegisterLineitemDocument.java
@@ -0,0 +1,23 @@
+package com.magustek.com.htxtpc.user.bean;
+
+import com.magustek.com.htxtpc.util.base.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@ApiModel(value = "PreRegisterLineitemDocument-用户(预)注册行项目-资料")
+@Data
+@Entity
+public class PreRegisterLineitemDocument extends BaseEntity {
+    @ApiModelProperty(value = "企业编码")
+    @Column(nullable = false) private String companyCode;
+    @ApiModelProperty(value = "用户名")
+    @Column(nullable = false) private String userName;
+    @ApiModelProperty(value = "资料流水号")
+    @Column(nullable = false) private String documentSerialNum;
+    @ApiModelProperty(value = "资料内容")
+    @Column() private String documentContent;
+}
