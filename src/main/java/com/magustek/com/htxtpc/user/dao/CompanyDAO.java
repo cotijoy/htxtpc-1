c0d3c6b80202e5afac85b2d8ad611fc42f9ae12f
root <whatreally@163.com>
2019-08-30 16:42:18 +0800

    增加CompanyDAO

---
+++ src/main/java/com/magustek/com/htxtpc/user/dao/CompanyDAO.java
@@ -0,0 +1,8 @@
+package com.magustek.com.htxtpc.user.dao;
+
+import com.magustek.com.htxtpc.user.bean.Company;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompanyDAO extends CrudRepository<Company,Long> {
+    Company findByCreditCode(String creditCode);
+}
