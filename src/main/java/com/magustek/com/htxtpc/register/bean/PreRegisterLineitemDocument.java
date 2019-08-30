6c3a6966633c0e3b12787c5d5a0c4b6a9284bbfb
root <whatreally@163.com>
2019-08-30 15:29:02 +0800

    增加无参数和全参数的构造方法

--- src/main/java/com/magustek/com/htxtpc/register/bean/PreRegisterLineitemDocument.java
+++ src/main/java/com/magustek/com/htxtpc/register/bean/PreRegisterLineitemDocument.java
@@ -3,7 +3,9 @@ package com.magustek.com.htxtpc.register.bean;
 import com.magustek.com.htxtpc.util.base.BaseEntity;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,6 +13,8 @@ import javax.persistence.Entity;
 @ApiModel(value = "PreRegisterLineitemDocument-用户(预)注册行项目-资料")
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class PreRegisterLineitemDocument extends BaseEntity {
     @ApiModelProperty(value = "企业编码")
     @Column(nullable = false) private String companyCode;
@@ -18,6 +22,11 @@ public class PreRegisterLineitemDocument extends BaseEntity {
     @Column(nullable = false) private String username;
     @ApiModelProperty(value = "资料流水号")
     @Column(nullable = false) private String documentSerialNum;
+    @ApiModelProperty(value = "资料大小")
+    @Column(nullable = false) private String documentSize;
+    @ApiModelProperty(value = "资料后缀")
+    @Column(nullable = false) private String documentSuffix;
     @ApiModelProperty(value = "资料内容")
     @Column() private String documentContent;
+
 }
