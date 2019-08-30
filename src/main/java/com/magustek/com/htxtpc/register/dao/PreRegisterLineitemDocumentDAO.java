bee63a5715bfd449ba4be9781760e9a7b7b71c00
root <whatreally@163.com>
2019-08-30 15:27:17 +0800

    修改

--- src/main/java/com/magustek/com/htxtpc/register/dao/PreRegisterLineitemDocumentDAO.java
+++ src/main/java/com/magustek/com/htxtpc/register/dao/PreRegisterLineitemDocumentDAO.java
@@ -1,6 +1,7 @@
 package com.magustek.com.htxtpc.register.dao;
 
+import com.magustek.com.htxtpc.register.bean.PreRegisterLineitemDocument;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PreRegisterLineitemDocumentDAO extends CrudRepository<PreRegisterLineitemDocumentDAO,Long> {
+public interface PreRegisterLineitemDocumentDAO extends CrudRepository<PreRegisterLineitemDocument,Long> {
 }
