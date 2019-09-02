78386b3ae5ca17503eebcf7f91c24906d289c0cf
root <whatreally@163.com>
2019-09-02 19:02:04 +0800

    修改

--- src/main/java/com/magustek/com/htxtpc/register/dao/RegisterHeaderDAO.java
+++ src/main/java/com/magustek/com/htxtpc/register/dao/RegisterHeaderDAO.java
@@ -1,7 +1,7 @@
 package com.magustek.com.htxtpc.register.dao;
 
-import com.magustek.com.htxtpc.register.bean.PreRegisterHeader;
+import com.magustek.com.htxtpc.register.bean.RegisterHeader;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RegisterHeaderDAO extends CrudRepository<PreRegisterHeader,Long> {
+public interface RegisterHeaderDAO extends CrudRepository<RegisterHeader,Long> {
 }
