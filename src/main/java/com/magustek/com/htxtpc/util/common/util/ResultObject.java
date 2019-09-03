9a378c4eaa4351ff33048908d8f3a2d0c3aaab19
root <whatreally@163.com>
2019-09-03 14:56:54 +0800

    增加返回结果结果类

---
+++ src/main/java/com/magustek/com/htxtpc/util/common/util/ResultObject.java
@@ -0,0 +1,26 @@
+package com.magustek.com.htxtpc.util.common.util;
+
+
+public class ResultObject {
+
+    /**
+     * status状态0:注册失败
+     */
+    public static final Integer status_0 = 0;
+
+    /**
+     * status状态0:注册成功
+     */
+    public static final Integer status_1 = 1;
+
+    /**
+     * status状态0:该用注册户正在被审核
+     */
+    public static final Integer status_2 = 2;
+
+    /**
+     * status状态0:该用户已经被注册
+     */
+    public static final Integer status_3 = 3;
+
+}
