f39c100ec1cf8fad37128fb4faec62dcf6e48960
root <whatreally@163.com>
2019-09-04 19:45:24 +0800

    修改

--- src/main/java/com/magustek/com/htxtpc/Swagger2.java
+++ src/main/java/com/magustek/com/htxtpc/Swagger2.java
@@ -19,7 +19,7 @@ public class Swagger2 {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.magustek.szjh"))
+                .apis(RequestHandlerSelectors.basePackage("com.magustek.htxtpc"))
                 .paths(PathSelectors.any())
                 .build();
     }
