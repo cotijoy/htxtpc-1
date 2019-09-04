65b1ea0c1febb8a8502f735f70b252bd24bec5e6
root <whatreally@163.com>
2019-09-04 19:47:18 +0800

    修改

--- src/main/java/com/magustek/com/htxtpc/register/dao/RegisterHeaderDAO.java
+++ src/main/java/com/magustek/com/htxtpc/register/dao/RegisterHeaderDAO.java
@@ -4,4 +4,6 @@ import com.magustek.com.htxtpc.register.bean.RegisterHeader;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RegisterHeaderDAO extends CrudRepository<RegisterHeader,Long> {
+    RegisterHeader findByPhoneNum (String phoneNum);
+    RegisterHeader findByUsernameAndPassword (String username, String password);
 }
