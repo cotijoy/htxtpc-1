6f5292ba0a31eeadd14899784b0b2baebabbd2a6
root <whatreally@163.com>
2019-09-04 19:46:50 +0800

    修改

--- src/main/java/com/magustek/com/htxtpc/register/controller/RegisterController.java
+++ src/main/java/com/magustek/com/htxtpc/register/controller/RegisterController.java
@@ -4,31 +4,37 @@ package com.magustek.com.htxtpc.register.controller;
 import com.magustek.com.htxtpc.register.bean.RegisterModel;
 import com.magustek.com.htxtpc.register.service.impl.RegisterModelServiceImpl;
 import com.magustek.com.htxtpc.util.base.BaseResponse;
+import com.magustek.com.htxtpc.util.common.util.ResultObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParams;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Api("用户注册")
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/register")
 public class RegisterController {
 
     @Autowired
     private RegisterModelServiceImpl registerModelService;
 
-
-    public Map<String, Object> register (HttpServletRequest request, RegisterModel registerModel) {
+    @RequestMapping(value = "/register.do")
+    public Map<String, Object> register (HttpServletRequest request, RegisterModel registerModel) throws Exception {
         CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver
                 (request.getSession().getServletContext());
         Map<String, Object> result = null;
@@ -41,6 +47,24 @@ public class RegisterController {
             log.info("手机号码为：" + registerModel.getPhoneNum() + "注册时缺少附件");
         }
         return result;
+    }
+
+    @RequestMapping(value = "/userLogin.do")
+    public Map<String, Object> login (HttpServletRequest request, HttpServletResponse response, String username, String password) throws Exception {
+        HttpSession session = request.getSession();
+        Map<String, Object> registResult = new HashMap<>();
+        if (session.getAttribute("user") == null) {  //用户未登录或session已经过期
+            registResult = registerModelService.userlogin(username, password);
+            if (registResult.get("user") == null) {
+                request.getRequestDispatcher("").forward(request, response);  //跳转到注册页面 具体路径未知
+            } else {
+                Integer status = (Integer) registResult.get("status");
+                /*if (status == ResultObject.)) {
+
+                }*/
+            }
+        }
+
     }
 
 }
