2a2b8c941dbe3e58598b710966bf0189a10a312a
root <whatreally@163.com>
2019-09-05 09:49:22 +0800

    修改属性

--- src/main/java/com/magustek/com/htxtpc/util/common/util/ResultObject.java
+++ src/main/java/com/magustek/com/htxtpc/util/common/util/ResultObject.java
@@ -6,21 +6,21 @@ public class ResultObject {
     /**
      * status状态0:注册失败
      */
-    public static final Integer status_0 = 0;
+    public static final Integer accountStatus_0 = 0;
 
     /**
      * status状态0:注册成功
      */
-    public static final Integer status_1 = 1;
+    public static final Integer accountStatus_1 = 1;
 
     /**
      * status状态0:该用注册户正在被审核
      */
-    public static final Integer status_2 = 2;
+    public static final Integer accountStatus_2 = 2;
 
     /**
      * status状态0:该用户已经被注册
      */
-    public static final Integer status_3 = 3;
+    public static final Integer accountStatus_3 = 3;
 
 }
