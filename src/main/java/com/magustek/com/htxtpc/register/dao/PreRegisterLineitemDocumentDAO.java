268bc0212f0ce761a3c72cb0c75dee7a90a63ed4
root <whatreally@163.com>
2019-09-19 16:33:51 +0800

    新增 通过用户名和公司编码查询的方法

--- src/main/java/com/magustek/com/htxtpc/register/dao/PreRegisterLineitemDocumentDAO.java
+++ src/main/java/com/magustek/com/htxtpc/register/dao/PreRegisterLineitemDocumentDAO.java
@@ -3,5 +3,8 @@ package com.magustek.com.htxtpc.register.dao;
 import com.magustek.com.htxtpc.register.bean.PreRegisterLineitemDocument;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PreRegisterLineitemDocumentDAO extends CrudRepository<PreRegisterLineitemDocument,Long> {
+    List<PreRegisterLineitemDocument>  findByCompanyCodeAndUsername(Long companyCode, String username);
 }
