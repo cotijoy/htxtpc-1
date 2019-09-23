b6273aa3ae3659480af4405d651d0acf3ecdea67
root <whatreally@163.com>
2019-09-23 10:43:33 +0800

    更改

--- src/main/java/com/magustek/com/htxtpc/user/bean/ReceiverAddressInformation.java
+++ src/main/java/com/magustek/com/htxtpc/user/bean/ReceiverAddressInformation.java
@@ -20,25 +20,31 @@ public class ReceiverAddressInformation extends BaseEntity {
     @ApiModelProperty(value="用户编码")
     @Column(nullable = false) private String userCode;
     @ApiModelProperty(value="国家")
-    @Column() private String nation;
+    @Column() private String nationTest;
     @ApiModelProperty(value="国家编码")
-    @Column() private String nationCode;
+    @Column() private String nation;
     @ApiModelProperty(value="省")
-    @Column() private String province;
+    @Column() private String provinceText;
     @ApiModelProperty(value="省编码")
-    @Column() private String provinceCode;
+    @Column() private String province;
     @ApiModelProperty(value="市")
-    @Column() private String city;
+    @Column() private String cityText;
     @ApiModelProperty(value="市编码")
-    @Column() private String cityCode;
+    @Column() private String city;
     @ApiModelProperty(value="区")
-    @Column() private String district;
+    @Column() private String areaText;
     @ApiModelProperty(value="区编码")
-    @Column() private String districtCode;
+    @Column() private String area;
+    @ApiModelProperty(value="镇")
+    @Column() private String townText;
+    @ApiModelProperty(value="镇编码")
+    @Column() private String town;
     @ApiModelProperty(value="详细地址")
     @Column() private String receiverAddress;
     @ApiModelProperty(value="收货人")
     @Column() private String receiver;
     @ApiModelProperty(value="手机号码")
     @Column() private String cellphoneNum;
+    @ApiModelProperty(value="默认标识")
+    @Column() private String defaultFlag;   //设为默认地址时，"X",否则为空
 }
