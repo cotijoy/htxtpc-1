7e1a8d179c4b9a24e143ef8fc715bbe2418eb4ba
root <whatreally@163.com>
2019-09-23 10:17:20 +0800

    新增

---
+++ src/main/java/com/magustek/com/htxtpc/manager/service/ReceiverAddressInformationService.java
@@ -0,0 +1,13 @@
+package com.magustek.com.htxtpc.manager.service;
+
+import com.magustek.com.htxtpc.manager.bean.ReceiverAddressInformationVO;
+import com.magustek.com.htxtpc.user.bean.ReceiverAddressInformation;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface ReceiverAddressInformationService {
+    ReceiverAddressInformation addOrUpdateReceiverAddressInformation (ReceiverAddressInformation receiverAddressInformation, String username);
+    void deleteReceiverAddressInformation (ReceiverAddressInformation receiverAddressInformation);
+    Page<ReceiverAddressInformation> searchReceiverAddressInformation (ReceiverAddressInformationVO receiverAddressInformationVO);
+}
