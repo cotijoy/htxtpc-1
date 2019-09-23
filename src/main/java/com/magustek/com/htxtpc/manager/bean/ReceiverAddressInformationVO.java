a327dec17f64f686d66ce096d4b93bc3669c4a31
root <whatreally@163.com>
2019-09-23 10:15:12 +0800

    新增

---
+++ src/main/java/com/magustek/com/htxtpc/manager/bean/ReceiverAddressInformationVO.java
@@ -0,0 +1,9 @@
+package com.magustek.com.htxtpc.manager.bean;
+
+import com.magustek.com.htxtpc.user.bean.ReceiverAddressInformation;
+import lombok.Data;
+
+@Data
+public class ReceiverAddressInformationVO extends ReceiverAddressInformation {
+    private String searching;
+}
